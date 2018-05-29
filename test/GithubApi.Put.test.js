9e217a7ba448876755a009852161b78e9d7d428c
Daniel Restrepo Mejía <daniel2894@gmail.com>
2018-05-29 13:36:36 -0500

    Put method (#13)
    * testing repos
    
    * test modified
    
    * test zip download of the repo
    
    * readme.md file in repository
    
    * download of files tested
    
    * use of subset added
    
    * follow user on github tested
    
    * verify the list of followed users
    
    * idempotent method
    
    * Fixup

---
+++ test/GithubApi.Put.test.js
@@ -0,0 +1,44 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect, assert } = require('chai');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'aperdomob';
+
+
+describe('Github API test PUT method', () => {
+  describe('follow users on Github', () => {
+    it('Should follow a user', () => {
+      agent.put(`${urlBase}/user/following/${githubUserName}`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.NO_CONTENT);
+          expect(response.body).to.be.empty();
+        });
+    });
+  });
+
+  describe('Working with the list of followers', () => {
+    let followQuery;
+    before(() => {
+      followQuery = agent.get(`${urlBase}/user/following`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          response.body.find(user => user.login === githubUserName);
+        });
+    });
+
+    it(`Should be followed ${githubUserName}`, () => {
+      followQuery.then(user => assert.exists(user));
+    });
+  });
+
+  describe('follow the user again', () => {
+    it('The method is idempotent', () => {
+      agent.put(`${urlBase}/user/following/${githubUserName}`)
+        .then((response) => {
+          expect(response.status).to.equal(statusCode.NO_CONTENT);
+          expect(response.body).to.be.empty();
+        });
+    });
+  });
+});
