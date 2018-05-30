e147b228923b10a3c293cc5dab884e16fe64f0eb
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-05-30 15:57:49 -0500

    test of getting an ammount of default users

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -4,18 +4,34 @@ const { expect } = require('chai');
 
 const urlBase = 'https://api.github.com';
 
-describe('For getting all users', () => {
+describe.only('For getting all users', () => {
   let queryTime;
+  let usersPagination;
 
-  before(() =>
-    agent.get(`${urlBase}/users`)
+  before(() => {
+    agent
+      .get(`${urlBase}/users`)
       .auth('token', process.env.ACCESS_TOKEN)
-      .use(responseTime((response, time) => {
+      .then((response) => {
+        usersPagination = response.body.length;
+      });
+
+    const usersQuery = agent
+      .get(`${urlBase}/users`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .use(responseTime((request, time) => {
         queryTime = time;
-      }))
-      .then(() => 0));
+      })).then(() => 0);
+
+    return usersQuery;
+  });
+
 
   it('Should have a time response below to 5 sec', () => {
     expect(queryTime).to.be.at.below(5000);
+  });
+
+  it('Should have 30 users by default', () => {
+    expect(usersPagination).to.equal(30);
   });
 });
