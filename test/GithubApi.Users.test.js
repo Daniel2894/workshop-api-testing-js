98989428eb0186deb40ea0bd33d06b81eb5b57be
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-05-30 14:02:20 -0500

    arrow functions modified

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -4,16 +4,16 @@ const { expect } = require('chai');
 
 const urlBase = 'https://api.github.com';
 
-describe.only('For getting all users', () => {
+describe('For getting all users', () => {
   let queryTime;
-  before(() => {
-    const allUsersQuery = agent.get(`${urlBase}/users`)
+
+  before(() =>
+    agent.get(`${urlBase}/users`)
       .auth('token', process.env.ACCESS_TOKEN)
       .use(responseTime((response, time) => {
         queryTime = time;
-      })).then(() => 0);
-    return allUsersQuery;
-  });
+      }))
+      .then(() => 0));
 
   it('Should have a time response below to 5 sec', () => {
     expect(queryTime).to.be.at.below(5000);
