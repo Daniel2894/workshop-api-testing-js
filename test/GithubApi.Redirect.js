1a43c78e078a7a973b3a49e654a2b380c1427f91
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-05-30 11:43:48 -0500

    arrow functions modified

--- test/GithubApi.Redirect.js
+++ test/GithubApi.Redirect.js
@@ -8,30 +8,29 @@ const newUrl = 'https://github.com/aperdomob/new-redirect-test';
 
 describe('Testing head method in the Github API', () => {
   describe('if i call an old URL', () => {
-    let query;
-    before(() => {
-      query = agent.head(oldUrl)
-        .auth('token', process.env.ACCESS_TOKEN);
-    });
+    let errorMsg;
+    before(() =>
+      agent.head(oldUrl)
+        .catch((error) => {
+          errorMsg = error;
+        }));
 
-    it('Should have redirect information', () =>
-      query.catch((error) => {
-        expect(error.response.headers.location).to.equal(newUrl);
-        expect(error.status).to.equal(statusCode.MOVED_PERMANENTLY);
-      }));
+    it('Should have redirect information', () => {
+      expect(errorMsg.response.headers.location).to.equal(newUrl);
+      expect(errorMsg.status).to.equal(statusCode.MOVED_PERMANENTLY);
+    });
   });
 
   describe('When get to the new URL', () => {
-    let redirectionQuery;
-    before(() => {
-      redirectionQuery = agent.get(oldUrl)
-        .auth('token', process.env.ACCESS_TOKEN);
-    });
+    let responseNewUrl;
+    before(() =>
+      agent.get(oldUrl)
+        .then((response) => {
+          responseNewUrl = response;
+        }));
 
     it('Should redirect correctly', () => {
-      redirectionQuery.then((response) => {
-        expect(response.status).to.equal(statusCode.OK);
-      });
+      expect(responseNewUrl.status).to.equal(statusCode.OK);
     });
   });
 });
